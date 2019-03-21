8954a4b09a5e7755f73f283dfa40e42d51a00293
Albert Stjärne <albert.stjarne@gmail.com>
2019-03-21 16:33:52 +0100

    adds bootstrap card to the contacts

--- src/App.js
+++ src/App.js
@@ -9,8 +9,10 @@ class App extends Component {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
+        <div className="container">
         <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
         <Contact name="Karen Smith" email="karen@gmail.com" phone="123-555-5555" />
+        </div>
       </div>
     );
   }
