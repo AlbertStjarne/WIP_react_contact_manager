d29e51ca4c573a2a1052e078032f0b8999142f0b
Albert Stjärne <albert.stjarne@gmail.com>
2019-03-28 16:51:12 +0100

    changing state in contact

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -30,13 +30,13 @@ state = {
     const { contacts } = this.state;
 
     return (
-      <div>
+      <React.Fragment>
         {contacts.map(contact => (
           <Contact 
             key={contact.id} 
             contact={contact} />
         ))}    
-      </div>
+      </React.Fragment>
     )
   }
 }
