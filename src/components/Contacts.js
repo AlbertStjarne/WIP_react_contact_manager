12dcd25f7928ec7e9c57ca3e7787da57a1f14ee7
Albert Stjärne <albert.stjarne@gmail.com>
2019-03-29 16:20:15 +0100

    fixing delete functionality with context reducer

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -4,18 +4,6 @@ import { Consumer } from '../context';
 
 class Contacts extends Component {
 
-
-deleteContact = (id) => {
-  const { contacts } = this.state;
-
-  const newContacts = contacts.filter(contact => contact.id != id);
-
-  this.setState({
-    contacts: newContacts
-  });
-};
-  
-
   render() {
     return (
       <Consumer>
@@ -27,7 +15,7 @@ deleteContact = (id) => {
                 <Contact 
                   key={contact.id} 
                   contact={contact}
-                  deleteClickHandler={this.deleteContact.bind(this, contact.id)} />
+                />
               ))}    
             </React.Fragment>      
           )
