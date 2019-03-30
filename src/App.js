361d654bcd86c50e49aafcf2b35f8f1e7510e0fb
Albert Stjärne <albert.stjarne@gmail.com>
2019-03-30 22:45:13 +0100

    adds NotFound page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Contacts from './components/contacts/Contacts';
 import Header from './components/layout/Header';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 import AddContact from './components/contacts/AddContact';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from './context';
@@ -21,6 +22,7 @@ class App extends Component {
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/contact/add" component={AddContact} />
                 <Route exact path="/about" component={About} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
