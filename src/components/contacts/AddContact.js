aa538564be50d061c2c0825cc3586ed038c33884
Albert Stjärne <albert.stjarne@gmail.com>
2019-03-30 11:41:24 +0100

    changing state on input in the form

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -5,7 +5,14 @@ class AddContact extends Component {
     name: '',
     email: '',
     phone: ''
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
   }
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   render() {
     const { name, email, phone } = this.state;
@@ -13,7 +20,7 @@ class AddContact extends Component {
       <div className="card mb-3">
         <div className="card-header">Add Contact</div>
         <div className="card-body">
-          <form>
+          <form onSubmit={this.onSubmit}>
             <div className="form-group">
               <label htmlFor="name">Name</label>
               <input 
@@ -21,7 +28,8 @@ class AddContact extends Component {
                 name="name" 
                 className="form-control form-control-lg"
                 placeholder="Enter name..." 
-                value={name}/>
+                value={name} 
+                onChange={this.onChange}/>
             </div>
             <div className="form-group">
               <label htmlFor="email">Email</label>
@@ -30,7 +38,8 @@ class AddContact extends Component {
                 name="email" 
                 className="form-control form-control-lg"
                 placeholder="Enter email..." 
-                value={email}/>
+                value={email} 
+                onChange={this.onChange}/>
             </div>
             <div className="form-group">
               <label htmlFor="name">Phone</label>
@@ -39,7 +48,8 @@ class AddContact extends Component {
                 name="phone" 
                 className="form-control form-control-lg"
                 placeholder="Enter phone..." 
-                value={phone}/>
+                value={phone} 
+                onChange={this.onChange}/>
             </div>
             <input type="submit" value="Add Contact" className="btn btn-light btn-block"/>
           </form>
