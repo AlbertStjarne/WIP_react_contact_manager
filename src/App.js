965059a3adcc3a70ec74ad466900630d151584ff
Albert Stjärne <albert.stjarne@gmail.com>
2019-03-31 17:16:42 +0200

    adds EditContact component and its page, edit icon with link on the home page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from './components/pages/About';
 import NotFound from './components/pages/NotFound';
 import Test from './components/test/Test';  // just for Test
 import AddContact from './components/contacts/AddContact';
+import EditContact from './components/contacts/EditContact';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from './context';
 
@@ -22,6 +23,7 @@ class App extends Component {
               <Switch>
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/test" component={Test} /> 
                 <Route component={NotFound} />
--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Consumer } from '../../context';
 import axios from 'axios';
@@ -33,7 +34,19 @@ class Contact extends Component {
                 {name}{' '}
                 <i onClick={() => this.setState({ showContactInfo: !this.state.showContactInfo })} className="fas fa-sort-down" 
                 style={{ cursor: 'pointer'}} />
-                <i className="fas fa-times" style={{ cursor: 'pointer', float: 'right', color: 'red'}} onClick={this.onDeleteClick.bind(this, id, dispatch)}/>
+                <i className="fas fa-times" style={{ cursor: 'pointer', float: 'right', color: 'red'}} onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                />
+                <Link to={`contact/edit/${id}`}>
+                  <i 
+                    className="fas fa-pencil-alt"
+                    style={{
+                      cursor: 'pointer',
+                      float: 'right',
+                      color: 'black',
+                      marginRight: '1rem'
+                    }}
+                  ></i>
+                </Link>
               </h4>
               {showContactInfo ? (        
                 <ul className="list-group">
