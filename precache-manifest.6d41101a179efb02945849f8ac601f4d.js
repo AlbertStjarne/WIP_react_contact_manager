1d5887df30a6d7c82b978a43d9b0322a80ed085b
Albert Stjärne <albert.stjarne@gmail.com>
2019-03-31 17:39:00 +0200

    Updates

---
+++ precache-manifest.6d41101a179efb02945849f8ac601f4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b549c23d59b2293a04d3",
+    "url": "/react_contact_manager/static/js/runtime~main.323bf5d9.js"
+  },
+  {
+    "revision": "6059e7bac68320eac5c3",
+    "url": "/react_contact_manager/static/js/main.35780c0f.chunk.js"
+  },
+  {
+    "revision": "de1c975b1f5a4b974b61",
+    "url": "/react_contact_manager/static/js/2.b120fcd8.chunk.js"
+  },
+  {
+    "revision": "6059e7bac68320eac5c3",
+    "url": "/react_contact_manager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "dc82d16493b0a51fdf5f386c199e0245",
+    "url": "/react_contact_manager/index.html"
+  }
+];
