50ce5508f76199271f6343f494d57fb672ab356e
Albert Stjärne <albert.stjarne@gmail.com>
2019-03-31 11:56:11 +0200

    adds test folder and Test file with different licfecycle methods

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Contacts from './components/contacts/Contacts';
 import Header from './components/layout/Header';
 import About from './components/pages/About';
 import NotFound from './components/pages/NotFound';
+import Test from './components/test/Test';
 import AddContact from './components/contacts/AddContact';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from './context';
@@ -22,6 +23,7 @@ class App extends Component {
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/contact/add" component={AddContact} />
                 <Route exact path="/about" component={About} />
+                <Route exact path="/test" component={Test} />
                 <Route component={NotFound} />
               </Switch>
             </div>
