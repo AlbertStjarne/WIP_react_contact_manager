5a51147e38c70a8410409783953fccdeb22cd8cf
Albert Stjärne <albert.stjarne@gmail.com>
2019-03-31 12:15:19 +0200

    install axios (npm install axios), adds GET request to jsonplaceholder example API

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 const Context = React.createContext();
 
@@ -21,28 +22,14 @@ const reducer = (state, action) => {
 
 export class Provider extends Component {
  state = {
-  contacts: [
-    {
-      id: 1,
-      name: 'John Doe',
-      email: 'jdoe@gmail.com',
-      phone: '555-555-5555'
-    },
-    {
-      id: 2,
-      name: 'Karen Williams',
-      email: 'kw@gmail.com',
-      phone: '123-555-5555'
-    },
-    {
-      id: 3,
-      name: 'Henry Johnson',
-      email: 'henryj@gmail.com',
-      phone: '222-333-5555'
-    }
-  ],
+  contacts: [],
   dispatch: action => this.setState(state => reducer(state, action))
  };
+
+ componentDidMount() {
+  axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(res => this.setState({contacts: res.data}))
+ }
 
  render() {
    return (
